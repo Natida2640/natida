160e6799cb96fe7c3c84c144add83313acf5c2e4
natida <natidaonsa399@gmail.com>
2018-01-13 14:35:40 +0700

    PutExtra Double

--- app/src/main/java/natida/sbc/ac/th/sbcmoneychange/fragment/CalcutateFragement.java
+++ app/src/main/java/natida/sbc/ac/th/sbcmoneychange/fragment/CalcutateFragement.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,11 +18,37 @@ import natida.sbc.ac.th.sbcmoneychange.R;
 
 public class CalcutateFragement extends Fragment{
 
+    private double aDouble;
+
+    public static CalcutateFragement calculateInstance(double factorDouble){
+        CalcutateFragement calcutateFragement = new CalcutateFragement();
+        Bundle bundle = new Bundle();
+        bundle.putDouble("Factor",factorDouble);
+        calcutateFragement.setArguments(bundle);
+
+        return calcutateFragement;
+    }
+
+
+
+
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
         //        Toolbar Controller
+        toolbarController();
+
+//        Get Value From Argument
+        aDouble = getArguments().getDouble("Factor");
+        Log.d("13Jan", "Factor ==>" + aDouble);
+
+
+
+    }   // Main Method
+
+    private void toolbarController() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);
         ((MainActivity)getActivity()).setSupportActionBar(toolbar);
 
@@ -37,10 +64,7 @@ public class CalcutateFragement extends Fragment{
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
-
-
-
-    }   // Main Method
+    }
 
     @Nullable
     @Override
